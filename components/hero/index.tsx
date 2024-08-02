72d90299372c6f2149cf13c92132e904d2fae7b4
Clue355 <dm.carlosluevano@gmail.com>
2024-08-01 21:03:39 -0500

    finished syling hero component

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -4,9 +4,9 @@ import Image from "next/image";
 
 export default function Hero() {
     return (
-        <section className="flex justify-center w-full bg-blackbg text-textlight h-[632px]">
-            <div className="flex 2-xl:w-[1113.4px] max-w-2xl pt-[128px]">
-                <div className="w-[398px] h-[346px] mr-[46.6px]">
+        <section className="flex justify-center w-full bg-[#131313] text-textlight h-[632px]">
+            <div className="flex 2-xl:w-[1113.4px]  ">
+                <div className="w-[398px] h-[346px] mr-[46.6px] pt-[128px]">
                     <p className="spacedtext text-[#7c7c7c] mb-[24px] font-normal">New Product</p>
                     <h1 className="mb-[24px]">XX99 Mark II Headphones</h1>
                     <p className="mb-[40px] font-medium text-[#c4c4c4]">
@@ -17,8 +17,13 @@ export default function Hero() {
                         <p className="subtitle">SEE PRODUCT</p>
                     </Button>
                 </div>
-                <div className="w-[708px]">
-                    <Image width={100} height={100} src="" alt="picture of XX99 Mark II Headphones" />
+                <div className="relative w-[600px] h-full">
+                    <Image
+                        className="absolute bottom-0"
+                        fill
+                        src="/images/Bitmap.png"
+                        alt="picture of XX99 Mark II Headphones"
+                    />
                 </div>
             </div>
         </section>
